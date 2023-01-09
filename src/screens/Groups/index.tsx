b136edc3faf22e3f6df948eae994184198d630be
Hélvio Filho <helviosvf@gmail.com>
2023-01-09 14:46:31 -0300

    Feat: created storage group and show groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -6,12 +6,35 @@ import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 import { GroupCard } from '@components/GroupCard';
 import { Loading } from '@components/Loading';
-import { FlatList } from 'react-native';
+import { Alert, FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { groupsGetAll } from '@storage/group/groupsGetAll';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { navigate } = useNavigation();
+
+  function fetchGroup() {
+    setIsLoading(true);
+    groupsGetAll()
+      .then((data) => {
+        setGroups(data);
+      })
+      .catch((e) => {
+        Alert.alert('Turmas', 'Não foi possível carregar as turmas');
+        console.log(e);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchGroup();
+  }, []));
 
   return (
     <Container>
@@ -34,9 +57,9 @@ export function Groups() {
                 onPress={() => { }}
               />
             )}
-            contentContainerStyle={groups.length === 0 && {flex: 1}}
+            contentContainerStyle={groups.length === 0 && { flex: 1 }}
             ListEmptyComponent={() => (
-              <ListEmpty 
+              <ListEmpty
                 message='Que tal cadastrar a primeira turma?'
               />
             )}
@@ -44,7 +67,7 @@ export function Groups() {
       }
       <Button
         title='Criar nova Turma'
-        onPress={() => { }}
+        onPress={() => navigate('newGroup')}
       />
     </Container>
   );
---
+++ src/storage/storageConfig.ts
@@ -0,0 +1,4 @@
+const GROUP_COLLECTION = '@fteam:groups';
+const PLAYER_COLLECTION = '@fteam:players';
+
+export { GROUP_COLLECTION, PLAYER_COLLECTION }
