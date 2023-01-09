c6d3425eb9ae02a4141ab8af0d3bd2044f945138
Hélvio Filho <helviosvf@gmail.com>
2023-01-09 14:44:46 -0300

    Feat: navigation on back button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { BackButton, BackIcon, Container, Logo } from './styles';
 
 import logoImg from '@assets/logo.png';
+import { useNavigation } from '@react-navigation/native';
 
 type HeaderProps = {
   showBackButton?: boolean;
 }
 
 export function Header({ showBackButton = false }: HeaderProps) {
+  const { navigate } = useNavigation();
+
   return (
     <Container>
       {
         showBackButton &&
-        <BackButton onPress={() => { }}>
+        <BackButton onPress={() => navigate('groups')}>
           <BackIcon />
         </BackButton>
       }
