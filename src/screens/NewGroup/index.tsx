7db9c7a6916daad133935e0c8c0399088259f3d6
Hélvio Filho <helviosvf@gmail.com>
2023-01-09 15:03:43 -0300

    Feat: creating new group

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -5,9 +5,34 @@ import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 
 import { Container, Content, Icon } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import { Alert } from 'react-native';
+import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from '@utils/AppError';
 
 export function NewGroup(){
   const [group, setGroup] = useState('');
+
+  const {navigate} = useNavigation();
+
+  function handleNew(){
+    if(group.trim().length === 0){
+      return Alert.alert('Novo grupo', 'Informe o nome da turma.');
+    }
+
+    groupCreate(group)
+    .then(() => {
+      navigate('players',{ group });
+    })
+    .catch((e) => {
+      if(e instanceof AppError){
+        Alert.alert('Novo Grupo', e.message);
+      }else{
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(e);
+      }
+    });
+  }
 
   return (
     <Container>
@@ -25,7 +50,7 @@ export function NewGroup(){
         <Button 
           title='Criar'
           style={{marginTop: 20}}
-          onPress={() => {}}
+          onPress={handleNew}
         />
       </Content>
     </Container>
